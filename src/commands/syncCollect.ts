b08e409c0f38eed23fb93fcfe1918f7b6dbe7a6f
ys152452 <362565210@qq.com>
2023-06-26 15:25:17 +0800

    异常处理

--- src/commands/syncCollect.ts
+++ src/commands/syncCollect.ts
@@ -1,8 +1,9 @@
+import { NGA } from "../nga";
 import * as vscode from "vscode";
 import Global from "../global";
 import * as cheerio from "cheerio";
 import http from "../http";
-import CustomProvider from '../providers/CustomProvider';
+import CustomProvider from "../providers/CustomProvider";
 
 const customProvider = new CustomProvider();
 /**
@@ -10,25 +11,37 @@ const customProvider = new CustomProvider();
  * @returns 返回是否成功添加
  */
 export default async function syncCollect(): Promise<boolean> {
-  const r = await http.get(`https://${Global.ngaURL}/nuke.php?__lib=forum_favor2&__act=forum_favor&__output=3&action=get`, { responseType: 'arraybuffer' });
-  const $ = cheerio.load(r.data);
-  const t = $('script').text().substring($('script').text().indexOf('=') + 1);
-  let data: any;
-  try {
-    data = JSON.parse(t).data[0];
-  }catch(err) {
-    console.log('error', err);
+  const cookie = Global.getCookie();
+  if (!cookie) {
+    vscode.window.showErrorMessage("请先登录");
     return false;
   }
-  console.log('收藏的分区', data);
+  const r = await http.get(`https://${Global.ngaURL}/nuke.php?__lib=forum_favor2&__act=forum_favor&__output=3&action=get`,{ responseType: "arraybuffer" });
+  const $ = cheerio.load(r.data);
+  const t = $("script").text().substring($("script").text().indexOf("=") + 1);
+  const d = JSON.parse(t);
+  console.log(t);
+  if(d.error) {
+    vscode.window.showErrorMessage(`获取数据失败, ${d.error[0]}`);
+    return false;
+  }
+  let data: any;
+  try {
+    data = d.data[0];
+  } catch (err) {
+    console.log("error", err);
+    vscode.window.showErrorMessage("同步失败");
+    return false;
+  }
+  console.log("收藏的分区", data);
   Object.keys(data).forEach(async (key) => {
-    let {fid, name = ''} = data[key];
+    let { fid, name = "" } = data[key];
     Global.addCustomNode({
       name: fid,
-      title: name
+      title: name,
     });
   });
   customProvider.refreshNodeList();
-  vscode.window.showInformationMessage('同步完成');
+  vscode.window.showInformationMessage("同步完成");
   return true;
 }
