d92a0585925a498e8603d0d4cc0353334238014a
DarrenB <bhl865931168@gmail.com>
2025-10-10 14:20:19 +0800

    v1.8.5

--- src/extension.ts
+++ src/extension.ts
@@ -65,7 +65,10 @@ export function activate(context: vscode.ExtensionContext) {
 	);
 
 	// 公共事件：在浏览器中打开
-	let cDisposable4 = vscode.commands.registerCommand('nga.viewInBrowser', (item: TreeNode) => vscode.env.openExternal(vscode.Uri.parse(item.link.slice(0,-17))));
+	let cDisposable4 = vscode.commands.registerCommand('nga.viewInBrowser', (item: TreeNode) => {
+		Global.addReadTid(parseInt(item.link.match(/tid=\d+/)![0].slice(4)));
+		vscode.env.openExternal(vscode.Uri.parse(item.link.slice(0,-17)));
+	});
 
 	// 公共事件：点击浏览帖子
 	let cDisposable5 = vscode.commands.registerCommand('nga.topicItemClick', (item: TreeNode) => topicItemClick(item));
